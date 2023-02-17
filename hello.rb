9fd193abb8b87346732bcb092a62aa3d4b7e31df
Jason <hylee.jason@gmail.com>
2023-02-18 00:18:52 +0800

    Mod code

--- hello.rb
+++ hello.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 
 get '/' do
-  DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  dayName = DAYNAMES[Time.now.wday]
+  DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
+              "Thursday", "Friday", "Saturday"]
+  dayname = DAYNAMES[Time.now.wday]
   "Hello, world! Happy #{dayname}."
 end
